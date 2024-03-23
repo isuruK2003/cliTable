c6ed175f3fe1533ac61f27188e667c3de549ec7a
isuru <isurukarunaratna01@gmail.com>
2024-03-24 00:54:54 +0530

    Removed unnecessary comments

--- table.py
+++ table.py
@@ -1,4 +1,4 @@
-def print_table(table, headers, seperator='|'):
+def print_table(table, headers, v_sep='|', h_sep='_'):
     if len(headers) == len(table[0]):
 
         full_table = [headers] + table # Generates a nested list where the first element is the header
@@ -21,87 +21,42 @@ def print_table(table, headers, seperator='|'):
             space = ' ' * (max_len - len(str(item)))
             formatted_header.append(f'{item}{space}')
         
-        # Prints the horizontal seperator line
-        formatted_header_str = f' {seperator} '.join(formatted_header)
+        formatted_header_str = f' {v_sep} '.join(formatted_header)
         print(formatted_header_str)
-        print('-' * len(formatted_header_str))
+
+        # Prints the horizontal seperator line
+        print(f'{h_sep}{v_sep}{h_sep}'.join( (h_sep * len(item) for item in formatted_header) ))
 
         # Printing table content
         for row_num, row in enumerate(table):
-
             row_num = str(row_num)
             # Initialize a list that contains space character(s) as the first element
             formatted_row = [f'{' ' * (max_row_num_length - len(row_num))}{row_num}']
-
             for item, max_len in zip(row, max_col_lengths):
                 item = str(item)
                 space = ' ' * (max_len - len(item))
                 formatted_item = f'{item}{space}'
                 formatted_row.append(formatted_item)
             
-            print(f' {seperator} '.join(formatted_row))
+            print(f' {v_sep} '.join(formatted_row))
 
-table = [['13/03/2024', 230.0, '2323', 'Income'], 
-         ['13/03/2024', 2356.0, 'Salary', 'Expense'], 
-         ['13/03/2024', 23.0, '23', 'Income'], 
-         ['13/03/2024', 23.0, '23', 'Expense'], 
-         ['13/03/2024', 23.0, '23', 'Income']]
+if __name__ == '__main__':
 
-headers = ['Date', 'Amount', 'Description', 'Type']
+    # Example
 
+    table = [
+                ['13/03/2024', 230.0, '2323', 'Income'], 
+                ['13/03/2024', 2356.0, 'Salary', 'Expense'], 
+                ['13/03/2024', 23.0, '23', 'Income'], 
+                ['13/03/2024', 23.0, '23', 'Expense'], 
+                ['13/03/2024', 23.0, '23', 'Income'],
+                ['13/03/2024', 23.0, '23', 'Income'],
+                ['13/03/2024', 23.0, '23', 'Income'],
+                ['13/03/2024', 23.0, '23', 'Income'],
+                ['13/03/2024', 23.0, '23', 'Income'],
+                ['13/03/2024', 23.0, '23', 'Income'],
+            ]
 
+    headers = ['Date', 'Amount', 'Description', 'Type']
 
-print_table(table, headers, '|')
-
-'''
-Simplest Alternative
-
-def view_transactions():
-    if transactions != []:
-        print('Format:', ', '.join(table_columns))
-        for num, transaction in enumerate(transactions):
-            print('')
-            print(f'Transaction {num}')
-            transaction = [str(item) for item in transaction]
-            print(', '.join(transaction))
-    else:
-        print('--- No transactions ---')
-
-version 01
-----------
-def print_table(data, headers=[]):
-    transpose = [[] for i in range(len(data[0]))]
-    for row in data:
-        for i, item in enumerate(row):
-            transpose[i].append(item)
-    
-    ## Compute maximum column lengths
-    max_col_lengths = []
-    for row in transpose:
-        col_lengths = [len(str(item)) for item in row]
-        max_col_lengths.append(max(col_lengths))
-
-    # Print the transposed data with proper spacing
-
-
-    for item, length in zip(headers, max_col_lengths):
-        space = " " * (length - len(str(item)))
-        print(f'  | {item}{space} ', end='')
-    print('|')
-
-    for index, row in enumerate(data):
-        for item, length in zip(row, max_col_lengths):
-            space = " " * (length - len(str(item)))
-            print(f'{index} | {item}{space} ', end='')
-        print('|')
-'''
-
-'''
-version simpler
-----------
-def print_table(table):
-    for row in table:
-        print("\t".join(map(str, row)))
-
-print_table(table)
-'''
+    print_table(table, headers, '│', '―')
